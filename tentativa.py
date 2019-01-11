2cf845b4f116100b391946c953f086d80e2e8e18
Sibele Vicente <sibelevicente1@hotmail.com>
2019-01-11 11:30:51 -0200

    FOI SO UM TESTE

---
+++ tentativa.py
@@ -0,0 +1,11 @@
+# i = 0
+# while (i < 9):
+# 	print('contagem: ', i)
+# 	i = i+1
+# print ("Acabou!")
+
+var = 0
+while (var < 9):
+	num = int(input("Digite um numero"))
+	print("Voce digitou: ",num)
+print("fim")
